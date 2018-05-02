1485bb7a587a4f162b91013c370cd15b34431034
Ashley Parker <aparker7698@att.net>
2018-05-02 12:54:27 -0400

    Added function to Header Component to determine whether or not user was logged in

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {loginWithUserData} from './helperFunctions/login';
 import {updateJWT} from './actions';
-import './stylesheets/login.css';  
 
 let mapStateToProps = (state) => {
     return {jwt: state.jwt}
